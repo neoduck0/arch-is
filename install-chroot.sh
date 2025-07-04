ee99938620cce669beb1209a9af5f77c36f89753
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-07-04 22:25:33 +0300

    chore: init

---
+++ install-chroot.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# quit on error
+set -e
+
+# go to project directory
+cd $(dirname $0)
+script_root=$(pwd)
+
+source helpers-chroot.sh
+
+mkinitcpio -p linux
+
+fnc_user_config
+
+fnc_gen_dirs
+
+fnc_set_locales
+
+fnc_install_grub
+
+fnc_config_pkgs
+
+fnc_gh_dotfiles neoduck0 dotfiles
+
+fnc_install_omz
+
+fnc_install_yay
+
+# delete project files
+cd /
+rm -rf $script_root
+
+clear
+echo "chroot script finished."
+exit 0
---
+++ pkgs.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+
+import os, sys
+
+WD = os.path.dirname(os.path.abspath(__file__))
+
+def main():
+    os.chdir(WD)
+
+    if len(sys.argv) == 1:
+        print("error: provide arguments.")
+        sys.exit(1)
+
+    match sys.argv[1]:
+        case "diff":
+            diff()
+        case "prompt":
+            prompt()
+        case _:
+            print("error: arguments invalid.")
+
+def diff():
+    selected = PROFILES["hyprland"]
+
+    os.system("""
+              pacman -Qeq > 1
+              """)
+
+    selected.sort()
+
+    with open("2", "w") as f:
+        for pkg in selected:
+            f.write(f"{pkg}\n")
+
+    os.system("""
+              diff 2 1
+              rm 1 2
+              """)
+
+def prompt():
+    profiles_list = list(PROFILES.keys())
+    while True:
+        os.system("clear")
+        for o in range(len(profiles_list)):
+            print(f"{profiles_list[o]}")
+        print()
+        profile = input("System Profile: ")
+        os.system("clear")
+        if profile in profiles_list:
+            gen_file(profile)
+            break
+
+
+def gen_file(profile):
+    pkgs = list(PROFILES[profile])
+    pkgs.sort()
+
+    with open("pkgs", "w") as f:
+        for pkg in pkgs:
+            f.write(pkg + "\n")
+
+BASIC = [
+        "base",
+        "base-devel",
+        "bash-language-server",
+        "btop",
+        "btrfs-progs",
+        "caligula",
+        "clang",
+        "curl",
+        "docker",
+        "docker-compose",
+        "dosfstools",
+        "efibootmgr" if not os.path.exists("/sys/firmware/uefi") else None,
+        "exfatprogs",
+        "fastfetch",
+        "fd",
+        "fuse2",
+        "fzf",
+        "git",
+        "go",
+        "gopls",
+        "gptfdisk",
+        "grub",
+        "intel-ucode",
+        "keyd",
+        "linux",
+        "linux-firmware",
+        "lua-language-server",
+        "man-db",
+        "neovim",
+        "networkmanager",
+        "nodejs",
+        "npm",
+        "ntfs-3g",
+        "openconnect",
+        "openssh",
+        "openvpn",
+        "pacman-contrib",
+        "pyright",
+        "python-pip",
+        "rclone",
+        "reflector",
+        "ripgrep",
+        "rust",
+        "sudo",
+        "typescript",
+        "typescript-language-server",
+        "udisks2",
+        "ufw",
+        "unzip",
+        "usbutils",
+        "vscode-css-languageserver",
+        "vscode-html-languageserver",
+        "wget",
+        "zip",
+        "zsh",
+        ]
+try:
+    BASIC.remove(None)
+except:
+    pass
+
+DESKTOP = [
+        "accountsservice",
+        "amberol",
+        "android-tools",
+        "audacity",
+        "bluez",
+        "bluez-utils",
+        "calf",
+        "deluge-gtk",
+        "dmidecode",
+        "dnsmasq",
+        "eartag",
+        "easyeffects",
+        "ffmpeg",
+        "firefox",
+        "flatpak",
+        "gnome-calculator",
+        "gnome-clocks",
+        "gnome-sound-recorder",
+        "gst-plugins-bad",
+        "gvfs-mtp",
+        "imagemagick",
+        "kitty",
+        "libreoffice-fresh",
+        "loupe",
+        "morphosis",
+        "mpv",
+        "nautilus",
+        "noto-fonts",
+        "noto-fonts-cjk",
+        "noto-fonts-emoji",
+        "obs-studio",
+        "papers",
+        "pdfarranger",
+        "pipewire",
+        "pipewire-alsa",
+        "pipewire-jack",
+        "pipewire-pulse",
+        "polkit-gnome",
+        "proton-vpn-gtk-app",
+        "pyside6",
+        "qemu-full",
+        "scrcpy",
+        "shotcut",
+        "snapshot",
+        "sof-firmware",
+        "solanum",
+        "spice-vdagent",
+        "swtpm",
+        "syncplay",
+        "ttf-jetbrains-mono-nerd",
+        "virt-manager",
+        "vulkan-icd-loader",
+        "vulkan-intel",
+        "wireplumber",
+        "yt-dlp",
+        "zed",
+        ]
+
+PROFILES = {
+        "server": BASIC,
+        "hyprland": [
+            "bluetui",
+            "brightnessctl",
+            "cliphist",
+            "hypridle",
+            "hyprland",
+            "hyprlock",
+            "hyprpaper",
+            "hyprpicker",
+            "hyprshot",
+            "mako",
+            "pavucontrol",
+            "power-profiles-daemon",
+            "qt5-wayland",
+            "qt6-wayland",
+            "rofi",
+            "uwsm",
+            "waybar",
+            "wev",
+            "wl-clip-persist",
+            "wl-clipboard",
+            "xdg-desktop-portal-gtk",
+            "xdg-desktop-portal-hyprland",
+            "xdg-user-dirs",
+            ]
+        + BASIC
+        + DESKTOP
+        }
+
+if __name__ == "__main__":
+    main()
