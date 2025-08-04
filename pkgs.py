a47fa92937b8bf9d5aa7165fe68de382950d4a5b
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-08-04 20:07:44 +0800

    feat: add pkgs.py with diff function

---
+++ pkgs.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+BASIC = [
+        "base",
+        "base-devel",
+        "bash-language-server",
+        "btop",
+        "btrfs-progs",
+        "clang",
+        "curl",
+        "docker",
+        "docker-compose",
+        "dosfstools",
+        "efibootmgr" if not os.path.exists("/sys/firmware/uefi") else None,
+        "exfatprogs",
+        "fastfetch",
+        "fd",
+        "fuse2",
+        "git",
+        "go",
+        "gopls",
+        "gptfdisk",
+        "grub",
+        "intel-ucode",
+        "keyd",
+        "linux",
+        "linux-firmware",
+        "lua-language-server",
+        "man-db",
+        "neovim",
+        "networkmanager",
+        "nodejs",
+        "npm",
+        "ntfs-3g",
+        "openssh",
+        "openvpn",
+        "pacman-contrib",
+        "pyright",
+        "python-pip",
+        "rclone",
+        "reflector",
+        "ripgrep",
+        "rust",
+        "sudo",
+        "udisks2",
+        "ufw",
+        "unzip",
+        "usbutils",
+        "wget",
+        "zip",
+        "zsh",
+        ]
+try:
+    BASIC.remove(None)
+except:
+    pass
+
+DESKTOP = [
+        "accountsservice",
+        "android-tools",
+        "bluez",
+        "bluez-utils",
+        "deluge-gtk",
+        "ffmpeg",
+        "flatpak",
+        "gvfs-mtp",
+        "imagemagick",
+        "noto-fonts",
+        "noto-fonts-emoji",
+        "pipewire",
+        "pipewire-alsa",
+        "pipewire-jack",
+        "pipewire-pulse",
+        "scrcpy",
+        "sof-firmware",
+        "ttf-jetbrains-mono-nerd",
+        "vulkan-icd-loader",
+        "vulkan-intel",
+        "wireplumber",
+        "yt-dlp"
+        ]
+
+PROFILES = {
+        "server": BASIC,
+        "hyprland": [
+            "amberol",
+            "bluetui",
+            "brightnessctl",
+            "cliphist",
+            "gnome-clocks",
+            "grim",
+            "gtk-engine-murrine",
+            "hypridle",
+            "hyprland",
+            "hyprlock",
+            "hyprpaper",
+            "hyprpicker",
+            "kitty",
+            "mako",
+            "morphosis",
+            "nautilus",
+            "pavucontrol",
+            "polkit-gnome",
+            "power-profiles-daemon",
+            "qt5-wayland",
+            "qt6-wayland",
+            "rofi-wayland",
+            "slurp",
+            "snapshot",
+            "solanum",
+            "uwsm",
+            "vlc",
+            "vlc-plugins-all",
+            "waybar",
+            "wev",
+            "wl-clip-persist",
+            "wl-clipboard",
+            "xdg-desktop-portal-gtk",
+            "xdg-desktop-portal-hyprland",
+            "xdg-user-dirs",
+            ]
+        + BASIC
+        + DESKTOP
+        }
+
+OPTS = {
+        "virtualization": [
+            "dmidecode",
+            "dnsmasq",
+            "qemu-full",
+            "spice-vdagent",
+            "swtpm",
+            "virt-manager"
+            ],
+        "office": [
+            "libreoffice-fresh"
+            ],
+        "vpn": [
+            "proton-vpn-gtk-app"
+            ]
+        }
+
+def diff(excluded_opts=[]):
+    selected = PROFILES["hyprland"]
+
+    os.system("""
+              pacman -Qeq > 1
+              """)
+
+    for OPT in OPTS:
+        if OPT not in excluded_opts:
+            selected += OPTS[OPT]
+
+    selected.sort()
+
+    with open("2", "w") as f:
+        for pkg in selected:
+            f.write(f"{pkg}\n")
+
+    os.system("""
+              diff 2 1
+              rm 1 2
+              """)
+
+diff()
