2622e3e0e0087d0013896973a6cc2fb984fda031
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-09-03 22:42:13 +0800

    refactor: remove systemd function

--- functions.sh
+++ functions.sh
@@ -47,32 +47,3 @@ function check_vars() {
         fi
     done
 }
-
-function systemd_services() {
-    if pacman -Q bluez &> /dev/null; then
-        systemctl enable bluetooth
-    fi
-    if pacman -Q libvirt &> /dev/null; then
-        systemctl enable libvirtd.socket
-    fi
-    if pacman -Q keyd &> /dev/null; then
-        cp ./resources/keyd.conf /etc/keyd/default.conf
-        systemctl enable keyd
-        keyd reload
-    fi
-    if pacman -Q firewalld &> /dev/null; then
-        systemctl enable firewalld
-    fi
-    if pacman -Q ufw &> /dev/null; then
-        systemctl enable ufw
-        ufw limit 22/tcp
-        ufw allow 80/tcp
-        ufw allow 443/tcp
-        ufw default deny incoming
-        ufw default allow outgoing
-        ufw enable
-    fi
-    if pacman -Q networkmanager &> /dev/null; then
-        systemctl enable NetworkManager
-    fi
-}
