7ea5021970365f1967beec7007111bcdb4192579
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-09-03 22:43:49 +0800

    style: add comments

--- install-chroot.sh
+++ install-chroot.sh
@@ -6,18 +6,20 @@ cd $(dirname $0)
 source functions.sh
 get_vars
 
+# locales
 ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
 hwclock --systohc
 locale-gen
 
+# grub
 if [ $disk_label = gpt ]; then
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 else
     grub-install --target=i386-pc /dev/$disk
 fi
-
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# systemd services
 if pacman -Q bluez &> /dev/null; then
     systemctl enable bluetooth
 fi
@@ -45,8 +47,10 @@ if pacman -Q networkmanager &> /dev/null; then
     systemctl enable NetworkManager
 fi
 
+# omz
 su --session-command='sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"' $user
 
+# yay
 git clone https://aur.archlinux.org/yay.git /home/$user/yay
 chown -R $user:$user /home/$user/yay
 cd /home/$user/yay
