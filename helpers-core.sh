2c9b5de21ec1474da11f294cf8dc647394101e8f
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-12-30 11:46:03 +0800

    fix

--- helpers-core.sh
+++ helpers-core.sh
@@ -132,6 +132,8 @@ function fnc_install_linux() {
 function fnc_support_encryption() {
     sed -i 's|block filesystems|block sd-encrypt filesystems|' /mnt/etc/mkinitcpio.conf
     sed -i "s|quiet|quiet rd.luks.name=$(blkid -s UUID -o value /dev/$root_part)=root root=/dev/mapper/root|" /mnt/etc/default/grub
+    touch /etc/vconsole.conf
+    mkinitcpio -p linux
 }
 
 function fnc_config_users() {
